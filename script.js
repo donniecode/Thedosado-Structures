2aa9b4000863fb58724e54da30f1d6bfaf83f531
Donald Zar <177225530+donniecode@users.noreply.github.com>
2024-08-01 23:12:13 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,9 +1,22 @@
-/* local location */
-/* const newwindow = new URL(window.location.href);
-newwindow.searchParams.set("name", "thedosadostructures");
-console.log(newwindow); */
+//mobile sidebar
+const menubtns = document.querySelector(".menu-btns");
+const hamburger = document.querySelector(".hamburger");
+const closemenu = document.querySelector(".close-menu");
+const sidebar = document.querySelector(".sidebar");
 
-/* back to top button */
+hamburger.addEventListener("click", function(){
+  sidebar.style.display="flex";
+  hamburger.style.display="none";
+  closemenu.style.display="block";
+})
+
+closemenu.addEventListener("click", function(){
+  sidebar.style.display="none";
+  hamburger.style.display="block";
+  closemenu.style.display="none";
+})
+
+// back to top button
 const toTop = document.querySelector('.back-to-top');
 
 window.addEventListener('scroll', function(){
